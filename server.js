6ccfa125835fb1d5c07082e6eb7d649265af3e00
Guilherme <devguicordeiro@gmail.com>
2023-04-26 18:11:40 -0300

    added password program local storage express and bcrypt library

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+const initializePassport = require("./passport-config");
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email)
+);
 
 const users = [];
 
@@ -26,7 +32,7 @@ app.post("/register", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
-      id: Date.now().toString,
+      id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       pasword: hashedPassword,
@@ -35,7 +41,6 @@ app.post("/register", async (req, res) => {
   } catch {
     res.redirect("/register");
   }
-  console.log(users);
 });
 
 app.listen(3000);
