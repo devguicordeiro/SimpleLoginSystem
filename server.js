299b3ac1c3b5a2611d8ae94af6461d862104f96c
Guilherme <devguicordeiro@gmail.com>
2023-04-26 14:32:34 -0300

    added routes and set pages

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const app = express();
 
 app.set("view-engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "Guilherme" });
 });
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", (req, res) => {});
 
 app.listen(3000);
